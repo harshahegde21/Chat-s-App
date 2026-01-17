26734f491ee9dad8140498e07ac44550ddade98a
Harsha Hegde <harshahegde10s@gmail.com>
2026-01-17 13:07:44 +0530

    fix: fixed and ready for deployment

--- client/frontend/src/socket/socket.js
+++ client/frontend/src/socket/socket.js
@@ -1,3 +1,9 @@
 import {io} from "socket.io-client"
-const socket = io("http://localhost:9878");
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:9878";
+
+const socket = io(BACKEND_URL,{
+    transports:["websocket"],
+    autoConnect:true
+});
 export default socket;
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const io = new Server(server,{cors:{
     origin:"http://localhost:5173"
 }})
 
-const PORT = 9878;
+const PORT = process.env.PORT || 9878;
 let messages = {}
 io.on("connection",(socket)=>{
     console.log(socket.id+" got connected");
